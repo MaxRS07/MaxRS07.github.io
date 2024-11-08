6a6af26ccea427a1ecd745998ab866d80244aeee
Max <Max@siebengartner.com>
2024-11-08 23:06:01 +0800

    restructure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import '../styles/Header.css';
+import MainLeft from './MainLeft';
+
+function Header() {
+    return (
+        <header className="App-header">
+            <MainLeft />
+        </header>
+    );
+}
+
+export default Header; 
---
+++ src/components/MainLeft.js
@@ -0,0 +1,15 @@
+import '../styles/MainLeft.css';
+
+function MainLeft() {
+    return (
+        <div className="App-main-left">
+            <h1>
+                Max Siebengartner <br />
+                <span className="App-colored-text">Human Things</span>
+            </h1>
+        </div>
+    );
+}
+
+
+export default MainLeft; 
